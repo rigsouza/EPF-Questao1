5ce29199d86514a7d67b05a27b16f158f52c8204
Ricardo <r.souzagoncalves@gmail.com>
2017-06-20 21:09:26 -0300

    EPF Questão 1

---
+++ scripts/processElementData.js
@@ -0,0 +1,47 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_moLzIVVPEeeBmtEwXAaWNg,_XNKaIFVQEeeBmtEwXAaWNgActivityDetail"]="./../../PluginTrabalho1/deliveryprocesses/resources/Verificar Abordagem do Teste_81DC2F71_44b8de9_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_moLzIVVPEeeBmtEwXAaWNg,_PV14kVVQEeeBmtEwXAaWNgActivity"]="./../../PluginTrabalho1/deliveryprocesses/resources/Definir Missão_2B10FA1C_ad805894_Activity.jpeg"
+contentPage.processPage.imageFiles["_moLzIVVPEeeBmtEwXAaWNg,_PV14kVVQEeeBmtEwXAaWNgActivityDetail"]="./../../PluginTrabalho1/deliveryprocesses/resources/Definir Missão_2B10FA1C_ad805894_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_moLzIVVPEeeBmtEwXAaWNgActivity"]="./../../PluginTrabalho1/deliveryprocesses/resources/ProcessoTrabalho1_D893FC24_d893fc24_Activity.jpeg"
+contentPage.processPage.imageFiles["_moLzIVVPEeeBmtEwXAaWNg,_bicYUVVQEeeBmtEwXAaWNgActivityDetail"]="./../../PluginTrabalho1/deliveryprocesses/resources/Testar e Avaliar_A284E2A6_24f4411e_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["_b4fSEVVWEeeBmtEwXAaWNg"] = ["Conjunto de Teste","PluginTrabalho1/deliveryprocesses/conjunto_de_teste_BDC7A31E.html"];
+contentPage.processPage.elementUrls["_auY2d1VWEeeBmtEwXAaWNg"] = ["Lista de Idéias de Testes","PluginTrabalho1/deliveryprocesses/lista_de_idias_de_testes_49070D28.html"];
+contentPage.processPage.elementUrls["_auY2dVVWEeeBmtEwXAaWNg"] = ["Caso de Teste","PluginTrabalho1/deliveryprocesses/caso_de_teste_F194234D.html"];
+contentPage.processPage.elementUrls["_jwzSIVVWEeeBmtEwXAaWNg"] = ["Design de Teste","PluginTrabalho1/deliveryprocesses/design_de_teste_68EA00C1.html"];
+contentPage.processPage.elementUrls["_jwzSJVVWEeeBmtEwXAaWNg"] = ["Arquitetura Para Automatização de Teste","PluginTrabalho1/deliveryprocesses/arquitetura_para_automatizao_de_teste_2D7BE2E0.html"];
+contentPage.processPage.elementUrls["_jwzSI1VWEeeBmtEwXAaWNg"] = ["Guia de Teste","PluginTrabalho1/deliveryprocesses/guia_de_teste_C05CEA9C.html"];
+contentPage.processPage.elementUrls["_auY2eVVWEeeBmtEwXAaWNg"] = ["Modelo de Análise de Carga de Trabalho","PluginTrabalho1/deliveryprocesses/modelo_de_anlise_de_carga_de_trabalho_B626056C.html"];
+contentPage.processPage.elementUrls["_m1UzEFVWEeeBmtEwXAaWNg"] = ["Lista de Idéias de Testes","PluginTrabalho1/deliveryprocesses/lista_de_idias_de_testes_48538F78.html"];
+contentPage.processPage.elementUrls["_moLzIVVPEeeBmtEwXAaWNg"] = ["Processo Entrega de Teste","PluginTrabalho1/deliveryprocesses/ProcessoTrabalho1_D893FC24.html"];
+contentPage.processPage.elementUrls["_dM-_0FVWEeeBmtEwXAaWNg"] = ["Executar Conjunto de Teste","PluginTrabalho1/deliveryprocesses/executar_conjunto_de_teste_EC217F3F.html"];
+contentPage.processPage.elementUrls["_XNKaIFVQEeeBmtEwXAaWNg"] = ["Verificar Abordagem do Teste","PluginTrabalho1/deliveryprocesses/Verificar Abordagem do Teste_81DC2F71.html"];
+contentPage.processPage.elementUrls["_auY2fVVWEeeBmtEwXAaWNg"] = ["Solicitação de Mudança","PluginTrabalho1/deliveryprocesses/solicitao_de_mudana_7AB7E78B.html"];
+contentPage.processPage.elementUrls["_bicYUVVQEeeBmtEwXAaWNg"] = ["Testar e Avaliar","PluginTrabalho1/deliveryprocesses/Testar e Avaliar_A284E2A6.html"];
+contentPage.processPage.elementUrls["_auY2cVVWEeeBmtEwXAaWNg"] = ["Design de Teste","PluginTrabalho1/deliveryprocesses/design_de_teste_2D02412E.html"];
+contentPage.processPage.elementUrls["_PV14kVVQEeeBmtEwXAaWNg"] = ["Definir Missão de Avaliação","PluginTrabalho1/deliveryprocesses/Definir Missão_2B10FA1C.html"];
+contentPage.processPage.elementUrls["_ezJsYFVWEeeBmtEwXAaWNg"] = ["Determinar Resultados do Teste","PluginTrabalho1/deliveryprocesses/determinar_resultados_do_teste_738C8459.html"];
+contentPage.processPage.elementUrls["_auY2c1VWEeeBmtEwXAaWNg"] = ["Build","PluginTrabalho1/deliveryprocesses/build_84752B09.html"];
+contentPage.processPage.elementUrls["_dM-_0VVWEeeBmtEwXAaWNg"] = ["Log de Testes","PluginTrabalho1/deliveryprocesses/log_de_testes_F6BF5F4F.html"];
+contentPage.processPage.elementUrls["_m1UzFFVWEeeBmtEwXAaWNg"] = ["Caso de Teste","PluginTrabalho1/deliveryprocesses/caso_de_teste_CE57197.html"];
+contentPage.processPage.elementUrls["_auY2cFVWEeeBmtEwXAaWNg"] = ["Implementar Teste","PluginTrabalho1/deliveryprocesses/implementar_teste_2264611E.html"];
+contentPage.processPage.elementUrls["_m1LCEFVWEeeBmtEwXAaWNg"] = ["Definir Detalhes do Teste","PluginTrabalho1/deliveryprocesses/definir_detalhes_do_teste_F40805AA.html"];
+contentPage.processPage.elementUrls["_auY2e1VWEeeBmtEwXAaWNg"] = ["Scripts de Teste","PluginTrabalho1/deliveryprocesses/scripts_de_teste_D98EF47.html"];
+contentPage.processPage.elementUrls["_ezJsYVVWEeeBmtEwXAaWNg"] = ["Resultados do Teste","PluginTrabalho1/deliveryprocesses/resultados_do_teste_7E2A6469.html"];
+contentPage.processPage.elementUrls["_jwzSJ1VWEeeBmtEwXAaWNg"] = ["Plano de Teste","PluginTrabalho1/deliveryprocesses/plano_de_teste_84EECCBB.html"];
+contentPage.processPage.elementUrls["_b4fSEFVWEeeBmtEwXAaWNg"] = ["Implementar Conjunto de Teste","PluginTrabalho1/deliveryprocesses/implementar_conjunto_de_teste_B329C30E.html"];
+contentPage.processPage.elementUrls["_jwzSIFVWEeeBmtEwXAaWNg"] = ["Definir Abordagem de Teste","PluginTrabalho1/deliveryprocesses/definir_abordagem_de_teste_5E4C20B1.html"];
+contentPage.processPage.elementUrls["_m1UzElVWEeeBmtEwXAaWNg"] = ["Plano de Teste","PluginTrabalho1/deliveryprocesses/plano_de_teste_418A839E.html"];
+contentPage.processPage.elementUrls["_m1UzFlVWEeeBmtEwXAaWNg"] = ["Modelo de Análise de Carga de Trabalho","PluginTrabalho1/deliveryprocesses/modelo_de_anlise_de_carga_de_trabalho_61C65BD.html"];
+contentPage.processPage.elementUrls["_m1LCEVVWEeeBmtEwXAaWNg"] = ["Design de Teste","PluginTrabalho1/deliveryprocesses/design_de_teste_FEA5E5BA.html"];
